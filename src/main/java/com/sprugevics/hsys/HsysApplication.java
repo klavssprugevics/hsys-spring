f4e855207085d21ae641e6bc2a8ba576c8a284cb
unknown <klavs.sprugevics@gmail.com>
2022-08-08 09:50:17 +0300

    Init

---
+++ src/main/java/com/sprugevics/hsys/HsysApplication.java
@@ -0,0 +1,13 @@
+package com.sprugevics.hsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprugevics/hsys/HsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprugevics.hsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
